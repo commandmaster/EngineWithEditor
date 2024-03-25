b6225950d4d339893d94e157b07839fb77027352
commandmaster <153877155+commandmaster@users.noreply.github.com>
2024-03-25 16:35:48 -0600

    so much work

--- editor/sketch.js
+++ editor/sketch.js
@@ -13,17 +13,58 @@ let editor = function(p5){
 class EditorEngine {
   constructor(p5){
     this.p5 = p5;
+    
   }
 
   Start(){
     this.p5.createCanvas(800, 600);
-    console.log("Editor Started");
+    this.Inspector = new Inspector(this.p5);
+    this.Inspector.Show();
   }
+
 
   Update(deltaTime){
     this.p5.background(0);
-    console.log("Editor Updated");
+
   }
+}
+
+class GuiElement {
+  constructor(p5){
+    this.p5 = p5;
+    this.datGui = new dat.GUI();
+    this.Hide();
+  }
+
+  Show(){
+    this.datGui.show();
+  }
+
+  Hide(){
+    this.datGui.hide();
+  }  
+}
+
+class Inspector extends GuiElement {
+  #inspectorFolder;
+  #selectedObject;
+
+  constructor(p5){
+    super(p5);
+  }
+
+  Show(){
+    super.Show();
+    this.#inspectorFolder = this.datGui.addFolder('Inspector');
+    this.#inspectorFolder.open();
+  }
+
+  Hide(){
+    super.Hide();
+
+  }
+
+
 }
 
 window.addEventListener("load", () => {
