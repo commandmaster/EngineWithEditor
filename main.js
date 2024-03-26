7cddcf7fdb402f3ef778bcf9b791cea81af278f1
commandmaster <153877155+commandmaster@users.noreply.github.com>
2024-03-25 21:49:49 -0600

    need to add config creation methods

--- main.js
+++ main.js
@@ -9,3 +9,126 @@ const prompt = require('electron-prompt');
 const Database = require('@bennettf/simpledb'); // My custom js json manipulation npm package
 
 const {dialog, app, BrowserWindow, Menu, shell, ipcMain,  nativeTheme, BrowserView} = electron;
+
+
+app.on('ready', function(){
+
+    nativeTheme.themeSource = 'dark';
+
+
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            preload: path.join(__dirname, 'editor/preload.js')
+          }
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'editor/index.html'),
+        protocol:'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', () => {
+        app.quit();
+    })
+
+   
+    
+    const mainMenuTemplate = [
+        {
+            label:'File',
+            submenu:[
+                {
+                    label: 'New Project',
+                    click(){
+
+                    }
+                },
+
+                {
+                    label: 'Save Project',
+                    click(){}
+                },
+
+                {
+                    label: 'Load Project',
+                    click(){}
+                },
+
+                {
+                    label: 'Quit',
+                    click(){app.quit();}
+                }
+            ]
+        },
+        {
+            label:'Start Game',
+            click(){
+                const gameMenuTemplate = [
+                    {
+                        label:"Quit",
+                        click(){app.quit();}
+                    },
+                    {
+                        label:"Restart",
+                        click(){
+                            mainWindow.reload();
+                        }
+                    },
+                    {
+                        label:"Open Dev Tools",
+                        click(){
+                            mainWindow.toggleDevTools();
+                        }
+                    },
+                    {
+                        label:"Close Dev Tools",
+                        click(){
+                            mainWindow.closeDevTools();
+                        }
+                    }
+                ];
+
+                const gameMenu = Menu.buildFromTemplate(gameMenuTemplate);
+
+                const startWindow = new BrowserWindow({
+                    webPreferences: {
+                        preload: path.join(__dirname, 'gameRunner/preload.js')
+                    }
+
+                });
+
+                startWindow.loadURL(url.format({
+                    pathname: path.join(__dirname, 'gameRunner/index.html'),
+                    protocol:'file:',
+                    slashes: true
+                }));
+
+                startWindow.setMenu(gameMenu);
+
+            }
+        },
+        {
+            label:'Dev Tools',
+            submenu:[
+                {
+                    label: 'Toggle Dev Tools',
+                    click(item, focusedWindow){
+                        focusedWindow.toggleDevTools();
+                    }
+                },
+                {
+                    role: 'reload'
+                }
+            ]
+            
+        }
+    ];
+
+
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    Menu.setApplicationMenu(mainMenu);
+});
+
